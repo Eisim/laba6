83c3c621b63b3f9408412b12f79fb36e887870dd
Eisim <koctik.w@mail.ru>
2023-06-07 19:49:32 +0300

    changed mygrammar.g4

--- include/myVisitor.h
+++ include/myVisitor.h
@@ -4,64 +4,29 @@
 #include "mygrammarVisitor.h"
 
 class  myVisitor : public mygrammarVisitor {
-     std::any visitInt(mygrammarParser::IntContext* context) {
-        std::cout << "Visit INT" << std::endl;
-       // int first = std::stoi(context->INT()->getText());
-        return 0;
+    std::any visitInt(mygrammarParser::IntContext* context);
 
-    };
+    std::any visitIntDOTint(mygrammarParser::IntDOTintContext* context) ;
 
-     std::any visitIntDOTint(mygrammarParser::IntDOTintContext* context) {
-        std::cout << "Visit intDOTint" << std::endl;
-        return 0;
-    };
+    std::any visitVarISexpr(mygrammarParser::VarISexprContext* context) ;
 
-     std::any visitVarISexpr(mygrammarParser::VarISexprContext* context) {
-         std::cout << "Visit varISexpr" << std::endl;
-         return 0;
+    std::any visitLexprR(mygrammarParser::LexprRContext* context) ;
 
-     };
+    std::any visitPrint_expr(mygrammarParser::Print_exprContext* context) ;
 
-     std::any visitLexprR(mygrammarParser::LexprRContext* context) {
-         std::cout << "Visit LexprR" << std::endl;
-         return 0;
-     };
+    std::any visitExprADDexpr(mygrammarParser::ExprADDexprContext* context)  ;
 
-     std::any visitExprADDexpr(mygrammarParser::ExprADDexprContext* context) {
-         std::cout << "Visit exprADDexpr" << std::endl;
-         return 0;
+    std::any visitExprMULexpr(mygrammarParser::ExprMULexprContext* context)  ;
 
-     };
+    std::any visitEvar(mygrammarParser::EvarContext* context)  ;
 
-     std::any visitExprMULexpr(mygrammarParser::ExprMULexprContext* context) {
-        std::cout << "Visit exprMULexpr" << std::endl;
-        return 0;
+    std::any visitEfloat(mygrammarParser::EfloatContext* context)  ;
 
-    };
+    std::any visitExpr_sep(mygrammarParser::Expr_sepContext* context)  ;
 
-     std::any visitEfloat(mygrammarParser::EfloatContext* context) {
-         std::cout << "Visit float" << std::endl;
-         return 0;
+    std::any visitProg_row(mygrammarParser::Prog_rowContext* context)  ;
 
-     };
-
-     std::any visitExpr_sep(mygrammarParser::Expr_sepContext* context) {
-         std::cout << "visit expr_sep" << std::endl;
-         return 0;
-
-     };
-
-     std::any visitProg_row(mygrammarParser::Prog_rowContext* context) {
-         std::cout << "Visit prog_row" << std::endl;
-         return 0;
-
-     };
-
-     std::any visitOneLineProg(mygrammarParser::OneLineProgContext* context) {
-         std::cout << "visit oneLineProg" << std::endl;
-         return 0;
-
-     };
+    std::any visitOneLineProg(mygrammarParser::OneLineProgContext* context)  ;
 
 public:
     std::vector<double> data;
